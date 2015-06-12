a4e21c591d269c9cfb0e9d98ee5b80a63ab0c5a9
Dio <primo.nicolas@outlook.be>
2015-06-12 11:58:17 +0200

    config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// préparation des constantes de connexion à mysql
+define("DB_SERVER","localhost" );
+define("DB_USER","root" );
+define("DB_PASSWORD","" );
+define("DB_NAME","prepatestjuint" );
